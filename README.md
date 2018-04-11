ef3204d7709b0f8d3b1b2f20abd654ab2637eba6
Tony <tony.zhouhj@gmail.com>
2018-04-11 15:08:50 +0800

    output context

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # AnticoneSize
-Calculate anti-cone size by   propagation delay , block rate and security threshold
+Calculate anti-cone size by  propagation delay , block rate and security threshold
+
+Equation:
+
+![Equation](https://www.zhihu.com/equation?tex=k%28D_%7Bmax%7D%2C%CE%B4%29+%3A%3D+min+%5Cleft+%5C%7B%5Chat%7Bk%7D+%5Cin+N+%3A+%281-e%5E%7B-2%5Ccdot+D_%7Bmax%7D%5Ccdot+%CE%BB%7D%29%5E%7B-1%7D%5Ccdot+%5Cleft+%28+%5Csum_%7Bj%3D%5Chat%7Bk%7D+%2B+1%7D%5E%7B%E2%88%9E%7D%7Be%5E%7B-2%5Ccdot+D_%7Bmax%7D%5Ccdot+%CE%BB%7D%5Ccdot+%5Cfrac%7B%282%5Ccdot+D_%7Bmax%7D%5Ccdot+%CE%BB%29%5Ej%7D%7Bj%21%7D%7D%5Cright+%29%5Cright+%5C%7D)
+
+Paper: https://eprint.iacr.org/2018/104.pdf
+
--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"math"
-	"log"
 	"flag"
+	"log"
+	"os"
 )
+
+var myLog = log.New(os.Stdout, "", 0)
 
 func main() {
 
@@ -16,12 +19,12 @@ func main() {
 
 	if *delay < 0 || *rate < 0 || *threshold < 0 {
 		flag.Usage()
-		log.Fatal("Keep parameters above zero!", *delay, *rate, *threshold)
+		myLog.Fatalf("keep parameters above zero! delay:%v rate:%v threshold:%v", *delay, *rate, *threshold)
 	}
 
 	if *threshold > 1.0 {
 		flag.Usage()
-		log.Fatal("Keep threshold under one!", *threshold)
+		myLog.Fatalf("keep threshold under one! threshold:%v", *threshold)
 	}
 
 	antiConeSize(*delay, *rate, *threshold)
@@ -33,14 +36,20 @@ func antiConeSize(_delay, _rate, _threshold float64) {
 	factor := 2 * _delay * _rate
 	coef := 1 / (math.Pow(math.E, factor) - 1)
 
-	log.Printf("_delay:%v _rate:%v _threshold:%v factor:%v coef:%v", _delay, _rate, _threshold, factor, coef)
+	myLog.Printf("_delay:%v _rate:%v _threshold:%v factor:%v coef:%v\n\n", _delay, _rate, _threshold, factor, coef)
 
 	sum := 0.0
 
-	for k := 1; k < 1000; k++ {
+	outLen := 10
+	kQueue := make([]float64, 0)
+
+	end := 1000
+
+	k := -1
+	for kk := 1; kk < end; kk++ {
 
 		sum = 0.0
-		for j := k + 1; j < k+100; j++ {
+		for j := kk + 1; j < kk+100; j++ {
 			xx := 1.0
 			for jj := 1; jj <= j; jj++ {
 				xx *= factor / float64(jj)
@@ -49,9 +58,23 @@ func antiConeSize(_delay, _rate, _threshold float64) {
 		}
 		sum *= coef
 
-		if sum < _threshold {
-			log.Println(k, sum)
-			break
+		if k < 0 {
+			if sum < _threshold {
+				for i := 0; i < len(kQueue); i++ {
+					myLog.Printf("kk=%v sum=%v", kk-(10-i), kQueue[i])
+				}
+				myLog.Printf("\n[MIN]kk=%v sum=%v\n\n", kk, sum)
+				k = kk
+				end = kk + 10 + 1
+			}
+
+			kQueue = append(kQueue, sum)
+			if kk > outLen {
+				kQueue = kQueue[1:]
+			}
+
+		} else {
+			myLog.Printf("kk=%v sum=%v", kk, sum)
 		}
 	}
 }
